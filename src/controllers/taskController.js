07c1cc3134d926d648a1d0b8e63d59bc0c115510
echa49er <bechaniq@charlotte.edu>
2025-10-28 23:32:02 -0400

    Add GET /tasks/:id endpoint functionality

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -5,6 +5,18 @@ export async function getTasks(req, res, next) {
   res.json(tasks);
 }
 
+export async function getTaskById(req, res, next) {
+  try {
+    const task = await taskService.getTaskById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+    res.json(task);
+  } catch (error) {
+    next(error);
+  }
+}
+
 export async function createTask(req, res, next) {
   const { title, completed } = req.body;
   const task = await taskService.createTask({ title, completed });
---
+++ src/middleware/validateTaskId.js
@@ -0,0 +1,10 @@
+import { param } from 'express-validator';
+import { checkValidationResults } from './handleValidationErrors.js';
+
+export const validateTaskId = [
+  param('id')
+    .isInt({ min: 1 })
+    .withMessage('ID must be a positive integer'),
+  
+  checkValidationResults,
+];
